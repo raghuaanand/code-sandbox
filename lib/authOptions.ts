6e4844e642a204fadb37689777cf89799d7840c6
Raghu Anand <raghuaanand@gmail.com>
2025-05-07 09:38:40 +0530

    fixed : fixed some changes and added google auth

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,18 +1,36 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 import type { NextAuthOptions } from 'next-auth';
 import { prisma } from './prisma';
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
+        })
+    ],
+    secret: process.env.NEXTAUTH_SECRET,       // :contentReference[oaicite:12]{index=12}
+    debug: process.env.NODE_ENV === "development",
+    callbacks: {
+      async signIn({ }) {
+        return true                              // :contentReference[oaicite:13]{index=13}
+      },
+    },
+    session: {
+        strategy: 'jwt',
+    },
 };
